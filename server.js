1687d500aa2c9cbfa4e3a47ed5189fa33c5a3812
sshong11 <73757024+sshong11@users.noreply.github.com>
2021-09-25 12:46:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 //Database
 //___________________
 // How to connect to the database either via heroku or locally
-const MONGODB_URI = 'mongodb+srv://sei:seipassword@sei.sogti.mongodb.net/test?retryWrites=true&w=majority'
+const MONGODB_URI = process.env.MONGODB_URI
 
 // Connect to Mongo &
 // Fix Depreciation Warnings from Mongoose
